b980853cf6d5255841c6427d58443eae2fb4f53e
miluckas <mishadarkluckas@gmail.com>
2021-03-20 14:59:44 +0500

    first_commit

--- second.php
+++ second.php
@@ -1,16 +1,27 @@
 <?php
-function generator_str($pr,$mass)
+function search_str($pr,$mass)
 {
-    foreach ($mass as $item)
-    {
-        if (mb_strpos($item,$pr)===0) {
+    if (is_array($mass) and is_string($pr)) {      // Проверяем данные
+        $generator = fn() => yield from $mass;     // Генерируем набор значений
+        $count= 0;
+        foreach ($generator() as $item) {
 
-            yield $item;
-
+            if (mb_strpos($item, $pr) === 0) {      // Выводим те, ктр начинаются с префиккса
+                echo $item . '<br>';
+                $count+=1;                          // Счетчик, если вдруг таких не окажется
+            }
         }
+        if ($count===0) echo "Таких строк не было";
     }
-
+    else{
+        echo "Ошибка во введенных данных";
+    }
 }
+
+
+$massiv = [1231,'asdf','qwexvc','ertyu','qwe'];
+$prefix = 'qwe';
+search_str($prefix,$massiv);
 
 
 
