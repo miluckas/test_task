78293d00420507f2e206f2069f17ea7e7e3f51d5
miluckas <mishadarkluckas@gmail.com>
2021-03-20 15:17:46 +0500

    first_commit

--- third.php
+++ third.php
@@ -2,8 +2,17 @@
 
 function amount($n, $m)
 {
-    return intdiv($m, $n);
-
+    if (is_int($n) and is_int($m) and ($n>0) and ($m>=0)) {
+        $ans = intdiv($m, $n);
+        return "Количество хипстеров - $n <br>Количество смузи - $m<br> Ответ - $ans";
+    }
+    else{
+        return 'Ошибка во входных данных';
+    }
 }
 
 
+$n = 7; // Количество хипстеров
+$m = 15; // Количество смузи
+
+echo amount($n,$m);
